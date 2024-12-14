214a2d88e4122a0a7abf407404efbf00e09efbe8
eric-armendariz <earmen3@illinois.edu>
2024-12-13 23:18:24 -0800

    Add dataset function

--- layer.py
+++ layer.py
@@ -1,9 +1,15 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+
+nnfs.init()
 
 # Each list represents connections to a neuron from prev layer of neurons
-X = [[1, 2, 3, 2.5],
-    [2.0, 5.0, -1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]]
+#X = [[1, 2, 3, 2.5],
+    #[2.0, 5.0, -1.0, 2.0],
+    #[-1.5, 2.7, 3.3, -0.8]]
+    
+X, y = spiral_data(100, 3)
 
 class LayerDense:
     def __init__(self, n_inputs, n_neurons):
@@ -16,10 +22,10 @@ class LayerDense:
 class ActivationReLU:
     def forward(self, inputs):
         #ReLU is 0 if num is <= 0 but num if num > 0
+        #Activation function is used to be able to fix non-linear functions
         self.output = np.maximum(0, inputs)
         
-layer1 = LayerDense(4, 5)
-layer2 = LayerDense(5, 2)
+layer1 = LayerDense(2, 5)
+activation1 = ActivationReLU()
 layer1.forward(X)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
